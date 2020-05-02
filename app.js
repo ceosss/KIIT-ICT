9c53968b65a6e6ba0208d09ffccc1f059eb9617b
ceosss <sswarajsamant@gmail.com>
2020-05-02 22:29:56 +0530

    mongoose uri

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var async = require("async");
 var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
 var cookieSession = require("cookie-session");
-var myModule = require("./export");
-const gcon = myModule.connectd;
 
 //SETTING COOKIE-SESSION
 app.use(
